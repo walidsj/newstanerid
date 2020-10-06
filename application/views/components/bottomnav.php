97d8f2396e0e13c51a97d714ddea621ed9711245
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2020-10-06 08:35:23 +0700

    Update bottomnav.php

--- application/views/components/bottomnav.php
+++ application/views/components/bottomnav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg desktop-navigation other-navbar p-0 d-block d-md-none fixed-bottom">
     <div class="container">
         <ul class="menu-mobile-bottom">
-            <li class="active">
+            <li class="<?= ($this->uri->segment(1) == '') ? 'active' : ''; ?>">
                 <a class="text-tertiary" href="<?= site_url(); ?>">
                     <div>
                         <img src="<?= base_url(); ?>assets/img/nav/unicorn.png" alt="Beranda" class="img" height="32">
@@ -11,7 +11,7 @@
                 </a>
             </li>
 
-            <li>
+            <li class="<?= ($this->uri->segment(1) == 'event') ? 'active' : ''; ?>">
                 <a class="text-dark" href="<?= site_url(); ?>event">
                     <div>
                         <img src="<?= base_url(); ?>assets/img/nav/unicorn2.png" alt="Beranda" class="img" height="32">
@@ -21,7 +21,7 @@
                 </a>
             </li>
 
-            <li>
+            <li class="<?= ($this->uri->segment(1) == 'berita') ? 'active' : ''; ?>">
                 <a class="text-dark" href="<?= site_url(); ?>berita">
                     <div>
                         <img src="<?= base_url(); ?>assets/img/nav/unicorn3.png" alt="Beranda" class="img" height="32">
@@ -31,7 +31,7 @@
                 </a>
             </li>
 
-            <li>
+            <li class="<?= ($this->uri->segment(1) == 'akun') ? 'active' : ''; ?>">
                 <a class="text-dark" href="javascript:void(0)" id="ShowMenuMobile2">
                     <div>
                         <img src="<?= base_url(); ?>assets/img/nav/unicorn4.png" alt="Beranda" class="img" height="32">
